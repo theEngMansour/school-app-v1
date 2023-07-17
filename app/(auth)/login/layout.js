f4693f143ec90a37136854dae11e67be0a636868
theEngMansour <mansour.yemen1@gmail.com>
2023-07-18 01:36:40 +0300

    add

--- app/(auth)/login/layout.js
+++ app/(auth)/login/layout.js
@@ -1,3 +1,13 @@
+import Image from "next/image";
+import logo from "@/public/logo.svg"
+
 export default function layout({ children }) {
-  return <div className="bg-gray-50 h-[100vh] flex justify-center items-center">{children}</div>;
+  return (
+    <div className="bg-gray-50 py-4 h-[100vh]">
+      <div className="mx-auto max-w-7xl px-2 lg:px-8">
+        <Image src={logo} className="sm:w-[360px] w-[240px] m-auto mb-4" alt="logo school app" />
+        <div className="mx-auto max-w-2xl lg:text-center">{children}</div>
+      </div>
+    </div>
+  );
 }
