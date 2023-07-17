07ec4568e8dd0f224d0e584da2eee92112c37eb8
theEngMansour <mansour.yemen1@gmail.com>
2023-07-18 01:37:58 +0300

    priority

--- app/(auth)/login/layout.js
+++ app/(auth)/login/layout.js
@@ -5,7 +5,7 @@ export default function layout({ children }) {
   return (
     <div className="bg-gray-50 py-4 h-[100vh]">
       <div className="mx-auto max-w-7xl px-2 lg:px-8">
-        <Image src={logo} className="sm:w-[360px] w-[240px] m-auto mb-4" alt="logo school app" />
+        <Image src={logo} className="sm:w-[360px] w-[240px] m-auto mb-4" alt="logo school app" priority />
         <div className="mx-auto max-w-2xl lg:text-center">{children}</div>
       </div>
     </div>
