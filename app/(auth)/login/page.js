0bfe8f2641a7ecfeb8e9f637fbd37cb9e6c6c5c9
theEngMansour <mansour.yemen1@gmail.com>
2023-07-18 01:36:33 +0300

    add

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,7 +1,6 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
-  Card,
   CardContent,
   CardDescription,
   CardFooter,
@@ -10,55 +9,48 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 export default function CardWithForm() {
   return (
-    <Card className="md:w-[750px] h-[550px] rounded-none border-0 text-right">
-      <CardHeader>
+    <div className="bg-white">
+      <CardHeader className="text-right">
         <CardTitle>إنشاء حساب</CardTitle>
-        <CardDescription>إضافة حساب جديد من خلال تعبئة البيانات</CardDescription>
+        <CardDescription>
+          إضافة حساب جديد من خلال تعبئة البيانات
+        </CardDescription>
       </CardHeader>
-      <CardContent>
-
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">الاسم</Label>
-              <Input
-                className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
-                id="name"
-                placeholder="إدخل الاسم الرباعي كاملاً"
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">البريد الإكتروني</Label>
-              <Input
-                className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
-                id="email"
-                type="email"
-                placeholder="إدخل البريد الإكتروني"
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">البريد الإكتروني</Label>
-              <Input
-                className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
-                id="password"
-                placeholder="إدخل البريد الإكتروني"
-              />
-            </div>
-
+      <CardContent className="text-right">
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">الاسم</Label>
+            <Input
+              className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
+              id="name"
+              placeholder="إدخل الاسم الرباعي كاملاً"
+            />
           </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="email">البريد الإكتروني</Label>
+            <Input
+              className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
+              id="email"
+              type="email"
+              placeholder="إدخل البريد الإكتروني"
+            />
+          </div>
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="password">كلمة السر</Label>
+            <Input
+              className="text-right focus-visible:ring-0 focus-visible:ring-offset-0 hover:border-gray-500"
+              id="password"
+              placeholder="إدخل كلمة السر"
+            />
+          </div>
+        </div>
       </CardContent>
       <CardFooter className="flex justify-center">
-        <Button className="w-[375px]">تسجيل دخول</Button>
+        <Button className="w-[375px] bg-green-700">تسجيل دخول</Button>
       </CardFooter>
-    </Card>
+    </div>
   );
 }
